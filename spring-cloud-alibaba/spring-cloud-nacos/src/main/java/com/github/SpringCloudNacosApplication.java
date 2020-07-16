5b0a8a66c2d37373b99f227a3b73001174db1790
hangs.zhang <hangs.zhang@qunar.com>
2020-07-16 19:20:48 +0800

    nacos

---
+++ spring-cloud-alibaba/spring-cloud-nacos/src/main/java/com/github/SpringCloudNacosApplication.java
@@ -0,0 +1,13 @@
+package com.github;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudNacosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudNacosApplication.class, args);
+    }
+
+}
