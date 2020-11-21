7cf816c55906b94c1e541e5b36ad29362f0d5881
plum-wine <1078504296@qq.com>
2020-11-22 00:17:00 +0800

    跟新pom.xml

--- spring-boot-app/spring-boot-web/src/main/java/com/github/web/controller/EnvController.java
+++ spring-boot-app/spring-boot-web/src/main/java/com/github/web/controller/EnvController.java
@@ -6,13 +6,21 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class EnvController {
-
-    @Value("${env:default}")
-    private String env;
-
-    @GetMapping("/env")
-    public String env() {
-        return env;
-    }
-
+	
+	@Value("${env:default}")
+	private String env;
+	
+	@Value("${server.tomcat.basedir}")
+	private String basedir;
+	
+	@GetMapping("/env")
+	public String env() {
+		return env;
+	}
+	
+	@GetMapping("/basedir")
+	public String basedir() {
+		return basedir;
+	}
+	
 }
