863507b59acc1a4a667614cab1aa823f181f9ea5
plum-wine <1078504296@qq.com>
2020-11-28 02:32:30 +0800

    database

--- spring-boot-app/spring-boot-web/src/main/java/com/github/web/response/BaseResult.java
+++ spring-boot-app/spring-boot-web/src/main/java/com/github/web/response/BaseResult.java
@@ -21,11 +21,11 @@ public class BaseResult<T> {
     private T data;
 
     public static <T> BaseResult<T> success(T obj) {
-        BaseResult<T> BaseResult = new BaseResult<>();
-        BaseResult.setCode(ResultEnum.SUCCESS.getCode());
-        BaseResult.setMsg(ResultEnum.SUCCESS.getMsg());
-        BaseResult.setData(obj);
-        return BaseResult;
+        BaseResult<T> baseResult = new BaseResult<>();
+        baseResult.setCode(ResultEnum.SUCCESS.getCode());
+        baseResult.setMsg(ResultEnum.SUCCESS.getMsg());
+        baseResult.setData(obj);
+        return baseResult;
     }
 
     public static <T> BaseResult<T> success() {
@@ -33,19 +33,19 @@ public class BaseResult<T> {
     }
 
     public static <T> BaseResult<T> error(Integer code, String msg) {
-        BaseResult<T> BaseResult = new BaseResult<>();
-        BaseResult.setCode(code);
-        BaseResult.setMsg(msg);
-        BaseResult.setData(null);
-        return BaseResult;
+        BaseResult<T> baseResult = new BaseResult<>();
+        baseResult.setCode(code);
+        baseResult.setMsg(msg);
+        baseResult.setData(null);
+        return baseResult;
     }
 
     public static <T> BaseResult<T> error(ResultEnum resultEnum) {
-        BaseResult<T> BaseResult = new BaseResult<>();
-        BaseResult.setCode(resultEnum.getCode());
-        BaseResult.setMsg(resultEnum.getMsg());
-        BaseResult.setData(null);
-        return BaseResult;
+        BaseResult<T> baseResult = new BaseResult<>();
+        baseResult.setCode(resultEnum.getCode());
+        baseResult.setMsg(resultEnum.getMsg());
+        baseResult.setData(null);
+        return baseResult;
     }
 
 }
