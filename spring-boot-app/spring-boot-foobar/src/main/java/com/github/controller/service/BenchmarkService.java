1a0a1633dbbc300fb3c2087b2f33b00a0527c73e
plum-wine <1078504296@qq.com>
2020-12-06 02:17:26 +0800

    benchmark

--- spring-boot-app/spring-boot-foobar/src/main/java/com/github/controller/service/BenchmarkService.java
+++ spring-boot-app/spring-boot-foobar/src/main/java/com/github/controller/service/BenchmarkService.java
@@ -3,6 +3,7 @@ package com.github.controller.service;
 import com.github.controller.domain.vo.BaseResult;
 import com.github.controller.domain.vo.UserVO;
 import com.github.controller.utils.JsonUtils;
+import com.google.common.base.Stopwatch;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -42,10 +43,15 @@ public class BenchmarkService {
     }
 
     public BaseResult<String> mockTask(String mark, boolean isCpuTask) {
+        BaseResult<String> result;
+        Stopwatch stopwatch = Stopwatch.createStarted();
         if (isCpuTask) {
-            return mockCpuTask(mark);
+            result = mockCpuTask(mark);
+        } else {
+            result = mockIOTask(mark);
         }
-        return mockIOTask(mark);
+        LOGGER.info("{} Task Mark:{}, cost time:{}ms", isCpuTask ? "Cpu" : "IO", mark, stopwatch.elapsed(TimeUnit.MILLISECONDS));
+        return result;
     }
 
     private BaseResult<String> mockIOTask(String mark) {
@@ -55,14 +61,16 @@ public class BenchmarkService {
             // do nothing
             LOGGER.error("Mock IO Task Error", e);
         }
-        LOGGER.info("IO Task Mark:{}", mark);
         return BaseResult.success(mark);
     }
 
     /**
      * 占同cpu 100ms
+     * 该方法不会去压榨cpu, 只是会让线程计算benchmarkTaskTime的时间
+     * 但是这个时间也算上了不被分配cpu的时间, 不是纯计算的时间
+     * 可以用来模拟即耗用io, 也耗用cpu的任务
      */
-    private BaseResult<String> mockCpuTask(String mark) {
+    private BaseResult<String> mockIOAndCpuTask(String mark) {
         long startTime = System.currentTimeMillis();
         // 占同cpu COST_TIME ms
         while ((System.currentTimeMillis() - startTime) <= benchmarkTaskTime) {
@@ -74,6 +82,27 @@ public class BenchmarkService {
             LOGGER.debug(JsonUtils.toJson(userVO));
         }
         LOGGER.info("Cpu Task Mark:{}", mark);
+        return BaseResult.success(mark);
+    }
+
+    /**
+     * 一段纯计算的任务, 无论是否分配cpu时间, 计算量都固定
+     * 在6c的Mac机器上, 单线程的情况下, 耗时100ms左右
+     */
+    private BaseResult<String> mockCpuTask(String mark) {
+        int num = 0;
+        int firstCycle = 1150;
+        int secondCycle = firstCycle * 100;
+        for (int i = 0; i < firstCycle; i++) {
+            for (int j = 0; j < secondCycle; j++) {
+                num += i * j;
+                if (num > 10000) {
+                    num += i;
+                }
+            }
+        }
+        // 欺骗编译器, 防止优化, 因为前面的代码计算的数据都没有被依赖
+        LOGGER.debug("num:{}", num);
         return BaseResult.success(mark);
     }
 
